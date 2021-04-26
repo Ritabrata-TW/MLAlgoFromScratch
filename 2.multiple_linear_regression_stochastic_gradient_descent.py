1a05fe4744af931c36a9153a9a4cd092b126be1b
ritabrata <ritabrata.m@rapido.bike>
2021-04-26 23:16:08 +0530

    [Rito] Add gradient descent using sgd for multiple linear regression

---
+++ 2.multiple_linear_regression_stochastic_gradient_descent.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+
+def predict(arr, coef):
+    yhat = coef[0]
+
+    for col_index in range(len(arr) - 1):
+        yhat += arr[col_index] * coef[col_index + 1]
+
+    return yhat
+
+
+def coefficients_sgd(df, l_rate, n_epoch):
+    coef = [0.0 for n in range(0, df.shape[1])]
+
+    for n in range(0, n_epoch):
+        sum_error = 0
+
+        for row in df.values:
+            yhat = predict(row, coef)
+            y = row[-1]
+            error = yhat - y
+            sum_error += error ** 2
+
+            coef[0] = coef[0] - error * l_rate
+
+            for col_index in range(len(row) - 1):
+                coef[col_index + 1] = coef[col_index + 1] - row[col_index] * l_rate * error
+
+            print(f"Epoch# = {n} lrate = {l_rate}, error = {sum_error}")
+    return coef
+
+
+def main():
+    df = pd.DataFrame({'x': [1, 2, 4, 3, 5], 'y': [1, 3, 3, 2, 5]})
+    l_rate = 0.001
+    n_epoch = 50
+
+    coef = coefficients_sgd(df, l_rate, n_epoch)
+    print(coef)
+
+
+if __name__ == '__main__':
+    main()
